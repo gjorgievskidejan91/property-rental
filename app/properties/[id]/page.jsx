b813f49d3ae487d03c8f2feb24a06093ca108002
unknown <gjorgievskidejan91@gmail.com>
2024-03-26 16:33:04 +0100

    Property page cleanup

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -2,24 +2,15 @@
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 import Link from "next/link";
-import {
-  FaBed,
-  FaBath,
-  FaRulerCombined,
-  FaTimes,
-  FaCheck,
-  FaPaperPlane,
-  FaBookmark,
-  FaShare,
-  FaMapMarker,
-  FaArrowLeft,
-} from "react-icons/fa";
+import { FaArrowLeft } from "react-icons/fa";
 import { fetchPropertyById } from "@/utils/requests";
 import PropertyHeaderImage from "@/components/PropertyHeaderImage";
 import PropertyDetails from "@/components/PropertyDetails";
 import BookmarkButton from "@/components/BookmarkButton";
 import Spinner from "@/components/Spinner";
 import PropertyImages from "@/components/PropertyImages";
+import ShareButtons from "@/components/ShareButton";
+import PropertyContactForm from "@/components/PropertyContactForm";
 
 const PropertyPage = () => {
   const { id } = useParams();
@@ -44,42 +35,42 @@ const PropertyPage = () => {
     }
   }, [id, property]);
 
-  if (!property && !loading)
-    return (
-      <h1 className="text-center text-2xl font-bold mt-10">
-        Property Not Found
-      </h1>
-    );
-
   return (
     <>
-      {loading && <Spinner loading={loading} />}
-      {!loading && property && (
-        <>
-          <PropertyHeaderImage image={property.images[0]} />
-          <section>
-            <div className="container m-auto py-6 px-6">
-              <Link
-                href="/properties"
-                className="text-blue-500 hover:text-blue-600 flex items-center"
-              >
-                <FaArrowLeft className="mr-2" /> Back to Properties
-              </Link>
-            </div>
-          </section>
+      {!loading && <PropertyHeaderImage image={property.images[0]} />}
 
-          <section className="bg-blue-50">
-            <div className="container m-auto py-10 px-6">
-              <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
-                <PropertyDetails property={property} />
-
-                <BookmarkButton />
-              </div>
-            </div>
-          </section>
-          <PropertyImages images={property.images} />
-        </>
+      {!loading && (
+        <section>
+          <div className="container m-auto py-6 px-6">
+            <Link
+              href="/properties"
+              className="text-blue-500 hover:text-blue-600 flex items-center"
+            >
+              <FaArrowLeft className="mr-2" /> Back to Properties
+            </Link>
+          </div>
+        </section>
       )}
+
+      {loading ? (
+        <Spinner loading={loading} />
+      ) : (
+        <section className="bg-blue-50">
+          <div className="container m-auto py-10 px-6">
+            <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+              <PropertyDetails property={property} />
+
+              {/* <!-- Sidebar --> */}
+              <aside className="space-y-4">
+                <BookmarkButton property={property} />
+                <ShareButtons property={property} />
+                <PropertyContactForm property={property} />
+              </aside>
+            </div>
+          </div>
+        </section>
+      )}
+      {!loading && <PropertyImages images={property.images} />}
     </>
   );
 };
--- components/PropertyDetails.jsx
+++ components/PropertyDetails.jsx
@@ -1,3 +1,4 @@
+import PropertyMap from "@/components/PropertyMap";
 import {
   FaBed,
   FaBath,
@@ -14,11 +15,9 @@ const PropertyDetails = ({ property }) => {
         <div className="text-gray-500 mb-4">{property.type}</div>
         <h1 className="text-3xl font-bold mb-4">{property.name}</h1>
         <div className="text-gray-500 mb-4 flex align-middle justify-center md:justify-start">
-          <i className="fa-solid fa-location-dot text-lg text-orange-700 mr-2"></i>
+          <FaMapMarker className="text-orange-700 mr-2 text-lg" />
           <p className="text-orange-700">
-            {" "}
-            {property.location.street}, {property.location.city}{" "}
-            {property.location.state}
+            {property.location.city}, {property.location.state}
           </p>
         </div>
 
@@ -26,10 +25,9 @@ const PropertyDetails = ({ property }) => {
           Rates & Options
         </h3>
         <div className="flex flex-col md:flex-row justify-around">
-          <div className="flex items-center justify-center mb-4 pb-4 md:pb-0">
+          <div className="flex items-center justify-center mb-4 border-b border-gray-200 md:border-b-0 pb-4 md:pb-0">
             <div className="text-gray-500 mr-2 font-bold">Nightly</div>
-
-            <div className="text-2xl font-bold text-blue-500">
+            <div className="text-2xl font-bold">
               {property.rates.nightly ? (
                 `$${property.rates.nightly.toLocaleString()}`
               ) : (
@@ -37,9 +35,8 @@ const PropertyDetails = ({ property }) => {
               )}
             </div>
           </div>
-          <div className="flex items-center justify-center mb-4 pb-4 md:pb-0">
+          <div className="flex items-center justify-center mb-4 border-b border-gray-200 md:border-b-0 pb-4 md:pb-0">
             <div className="text-gray-500 mr-2 font-bold">Weekly</div>
-
             <div className="text-2xl font-bold text-blue-500">
               {property.rates.weekly ? (
                 `$${property.rates.weekly.toLocaleString()}`
@@ -50,7 +47,6 @@ const PropertyDetails = ({ property }) => {
           </div>
           <div className="flex items-center justify-center mb-4 pb-4 md:pb-0">
             <div className="text-gray-500 mr-2 font-bold">Monthly</div>
-
             <div className="text-2xl font-bold text-blue-500">
               {property.rates.monthly ? (
                 `$${property.rates.monthly.toLocaleString()}`
@@ -61,26 +57,26 @@ const PropertyDetails = ({ property }) => {
           </div>
         </div>
       </div>
+
       <div className="bg-white p-6 rounded-lg shadow-md mt-6">
         <h3 className="text-lg font-bold mb-6">Description & Details</h3>
         <div className="flex justify-center gap-4 text-blue-500 mb-4 text-xl space-x-9">
           <p>
-            <FaBed className="inline-block mr-2" />
-            {property.beds}
+            <FaBed className="inline-block mr-2" /> 3{" "}
             <span className="hidden sm:inline">Beds</span>
           </p>
           <p>
-            <FaBath className="inline-block mr-2" /> {property.baths}
+            <FaBath className="inline-block mr-2" /> 2{" "}
             <span className="hidden sm:inline">Baths</span>
           </p>
           <p>
             <FaRulerCombined className="inline-block mr-2" />
-            {property.square_feet}{" "}
-            <span className="hidden sm:inline">sqft</span>
+            1,500 <span className="hidden sm:inline">sqft</span>
           </p>
         </div>
         <p className="text-gray-500 mb-4">{property.description}</p>
       </div>
+
       <div className="bg-white p-6 rounded-lg shadow-md mt-6">
         <h3 className="text-lg font-bold mb-6">Amenities</h3>
 
@@ -93,8 +89,11 @@ const PropertyDetails = ({ property }) => {
           ))}
         </ul>
       </div>
+
+      <div className="bg-white p-6 rounded-lg shadow-md mt-6">
+        <PropertyMap property={property} />
+      </div>
     </main>
   );
 };
-
 export default PropertyDetails;
